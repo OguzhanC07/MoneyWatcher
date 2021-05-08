7198df1637ad9dcb1f31677bd027c673335b863b
onurgoz <onurgoz98@gmail.com>
2021-05-08 19:26:12 +0300

    Added dbcontext

---
+++ src/MoneyWatcher.DataAccess/Concrete/EntityFrameworkCore/Context/MoneyWatcherDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyWatcher.DataAccess.Concrete.Mapping;
+using MoneyWatcher.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyWatcher.DataAccess.Concrete.EntityFrameworkCore.Context
+{
+    public class MoneyWatcherDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BudgetDateMap());
+            modelBuilder.ApplyConfiguration(new BudgetMap());
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Budget> Budgets { get; set; }
+        public DbSet<BudgetDate> BudgetDates { get; set; }
+    }
+}
