a52139da2e49ee0aa9ed5a5e4448070cd13a0a33
Oğuzhan Can <59476485+OguzhanC07@users.noreply.github.com>
2021-05-22 18:13:04 +0300

    Added budget

---
+++ src/MoneyWatcher.Businness/Utils/Dtos/BudgetDto/BudgetAddDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MoneyWatcher.Entities.Concrete;
+
+namespace MoneyWatcher.Businness.Utils.Dtos.BudgetDto
+{
+    public class BudgetAddDto
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Detail { get; set; }
+        public bool BudgetType { get; set; }
+        public Guid UserId { get; set; }
+        public int CategoryId { get; set; }
+        public BudgetDate BudgetDate { get; set; }
+    }
+}
